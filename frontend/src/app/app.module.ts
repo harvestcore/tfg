d100b779b991a9420a4772c44ea0097b565bf384
harvestcore <aagomezies@gmail.com>
2020-05-03 18:51:20 +0200

    Implement routing #70

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import {AuthService} from '../services/auth.service';
 import {CustomerService} from '../services/customer.service';
 import {DeployService} from '../services/deploy.service';
@@ -12,6 +13,7 @@ import {PlaybookService} from '../services/playbook.service';
 import {ProvisionService} from '../services/provision.service';
 import {StatusService} from '../services/status.service';
 import {UserService} from '../services/user.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const providers = [
   AuthService,
@@ -27,9 +29,11 @@ const providers = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     HttpClientModule
   ],
