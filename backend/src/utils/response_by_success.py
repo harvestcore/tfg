1d9a3a12f71980f964614280f055aec9df8d9f01
harvestcore <aagomezies@gmail.com>
2020-05-20 23:27:06 +0200

    Change status code #81

--- backend/src/utils/response_by_success.py
+++ backend/src/utils/response_by_success.py
@@ -3,6 +3,6 @@ def response_by_success(success, is_remove=False):
         code = 200 if not is_remove else 204
         return {
             'ok': True,
-            'message': 'Operation completed successfully.'
+            'message': 'Operation completed successfully'
         }, code
-    return {'ok': False, 'message': 'Unsuccessful operation.'}, 500
+    return {'ok': False, 'message': 'Unsuccessful operation'}, 422
